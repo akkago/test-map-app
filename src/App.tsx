b972a9f70c31083d37f5a4c25c8b878ad098484d
akkado <riabov.s@outlook.com>
2022-10-09 15:44:40 +0300

    styles fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
     <div className="App">
       <div className='container'>
         <div className='body'>
-          <ResizePanel direction="e" style={{ flexGrow: '1' }} >
-            <div className='sidebar withMargin panel'>
+          <ResizePanel
+            direction="e"
+            style={{ flexGrow: '1' }}
+            borderClass='customResizeBorder'
+          >
+            <div className='sidebar'>
               <TrackTable />
             </div>
           </ResizePanel>
-          <div className='content panel'>  <TrackMap /></div>
+          <TrackMap />
         </div>
       </div>
     </div>
